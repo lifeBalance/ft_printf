69b8f37c7ad141550d327f675e7dac23cd4e42e7
rodrodri <rodrodri@student.hive.fi>
2022-01-18 13:10:46 +0200

    to_float: finish logic for printing when precision is 0

--- srcs/pf_to_float_converter.c
+++ srcs/pf_to_float_converter.c
@@ -6,7 +6,7 @@
 /*   By: rodrodri <rodrodri@student.hive.fi >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 12:24:46 by rodrodri          #+#    #+#             */
-/*   Updated: 2022/01/17 23:14:18 by rodrodri         ###   ########.fr       */
+/*   Updated: 2022/01/18 13:09:02 by rodrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,11 @@ typedef struct s_part {
 }	t_float;
 
 static t_float	split_float(double num, int pos);
-static int		no_precision(t_float *n, t_spec *spec);
+static int		no_precision_right(t_float *n, t_spec *spec);
+static int		no_precision_left(t_float *n, t_spec *spec);
+static int		add_spaces(t_float *n, t_spec *spec);
+static int		handle_precision(t_float *n, t_spec *spec);
+
 // static int		print_posit_float(t_float *n, t_spec *spec);
 
 /*
@@ -42,41 +46,96 @@ int	to_float(va_list data_args, t_spec *spec)
 		spec->prec = DEFAULT_PREC;
 	n = split_float(va_arg(data_args, double), spec->prec);
 	if (spec->prec == 0)
-		ret += no_precision(&n, spec);
-	else
 	{
-		if (n.integer < 0)
-		{
-			ret += ft_putchar('-');
-			n.integer *= -1;
-		}
-		ret += put_ull_base(n.integer, DECDIGITS);
-		ret += ft_putchar('.');
-		ret += put_ull_base(n.decimal, DECDIGITS);
-		if (amount_digits(n.decimal, 10) < spec->prec)
-			ret += putstr_repeat("0", spec->prec - amount_digits(n.decimal, 10));
+		if (test_bit(MINUS, spec->flags))
+			ret += no_precision_left(&n, spec);
+		else
+			ret += no_precision_right(&n, spec);
 	}
+	else
+		ret += handle_precision(&n, spec);
 	return (ret);
 }
 
-static int	no_precision(t_float *n, t_spec *spec)
+static int	add_spaces(t_float *n, t_spec *spec)
+{
+	int	ret;
+	int	amount;
+
+	ret = 0;
+	amount = spec->width - amount_digits(n->integer, 10);
+	if (n->integer < 0 || test_bit(PLUS, spec->flags) || \
+		test_bit(SPACE, spec->flags))
+		ret += putstr_repeat(" ", amount - 1);
+	else
+		ret += putstr_repeat(" ", amount);
+	return (ret);
+}
+
+static int	handle_precision(t_float *n, t_spec *spec)
 {
 	int		ret;
 
-	if (n->integer < 0 || test_bit(PLUS, spec->flags) || test_bit(SPACE, spec->flags))
-		putstr_repeat(" ", spec->width - amount_digits(n->integer, 10) - 1);
-	else
-		putstr_repeat(" ", spec->width - amount_digits(n->integer, 10));
 	ret = 0;
 	if (n->integer < 0)
 	{
 		ret += ft_putchar('-');
-		n->integer = n->integer *= -1;
-	} else if (test_bit(PLUS, spec->flags))
+		n->integer *= -1;
+	}
+	ret += put_ull_base(n->integer, DECDIGITS);
+	ret += ft_putchar('.');
+	ret += put_ull_base(n->decimal, DECDIGITS);
+	if (amount_digits(n->decimal, 10) < spec->prec)
+		ret += putstr_repeat("0", spec->prec - amount_digits(n->decimal, 10));
+	return (ret);
+}
+
+static int	no_precision_right(t_float *n, t_spec *spec)
+{
+	int		ret;
+
+	ret = 0;
+	if (!test_bit(ZERO, spec->flags))
+		ret += add_spaces(n, spec);
+	if (n->integer < 0)
+		ret += ft_putchar('-');
+	else if (test_bit(PLUS, spec->flags))
 		ret += ft_putchar('+');
 	else if (test_bit(SPACE, spec->flags))
 		ret += ft_putchar(' ');
+	if (test_bit(ZERO, spec->flags))
+	{
+		if (n->integer < 0 || test_bit(PLUS, spec->flags) || \
+			test_bit(SPACE, spec->flags))
+			ret += putstr_repeat("0", spec->width - \
+				amount_digits(n->integer, 10) - 1);
+		else
+			ret += putstr_repeat("0", spec->width - \
+				amount_digits(n->integer, 10));
+	}
+	if (n->integer < 0)
+		n->integer *= -1;
 	ret += put_ull_base(n->integer, DECDIGITS);
+	return (ret);
+}
+
+static int	no_precision_left(t_float *n, t_spec *spec)
+{
+	int	ret;
+	int	sign;
+
+	sign = 1;
+	ret = 0;
+	if (n->integer < 0)
+		ret += ft_putchar('-');
+	else if (test_bit(PLUS, spec->flags))
+		ret += ft_putchar('+');
+	else if (test_bit(SPACE, spec->flags))
+		ret += ft_putchar(' ');
+	if (n->integer < 0)
+		sign *= -1;
+	ret += put_ull_base(n->integer * sign, DECDIGITS);
+	ret += add_spaces(n, spec);
 	return (ret);
 }
 
