dd35b4c705f8cc75785712349b40130b1eab0457
Javier <javi@grey.local>
2022-01-22 20:55:41 +0200

    add 'set_pad_char' to set the character for padding (0 or space) in 'print_padding'

--- srcs/pf_converter_utils3.c
+++ srcs/pf_converter_utils3.c
@@ -6,7 +6,7 @@
 /*   By: rodrodri <rodrodri@student.hive.fi >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 12:24:46 by rodrodri          #+#    #+#             */
-/*   Updated: 2022/01/22 18:33:27 by rodrodri         ###   ########.fr       */
+/*   Updated: 2022/01/22 20:51:10 by rodrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ static int	print_prefix(long long n, t_spec *spec);
 static int	print_padding(long long n, t_spec *spec);
 static int	print_number(long long n, t_spec *spec);
 static int	assemble_num(long long n, t_spec *spec);
+void		set_pad_char(t_spec *spec, char *pad_char);
 
 /*
 **	Prints a number in any given base, according to some specifications
@@ -73,10 +74,7 @@ static int	print_padding(long long n, t_spec *spec)
 
 	ret = 0;
 	amount = 0;
-	if (test_bit(ZERO, spec->flags) && spec->prec == NOT_SET)
-		ft_strcpy(pad_char, "0\0");
-	else
-		ft_strcpy(pad_char, " \0");
+	set_pad_char(spec, pad_char);
 	if ((test_bit(SHARP, spec->flags) && n > 0) && \
 		(spec->specifier == LOWHEX || spec->specifier == UPPHEX))
 		amount -= 2;
@@ -85,7 +83,7 @@ static int	print_padding(long long n, t_spec *spec)
 		(test_bit(PLUS, spec->flags) && n >= 0) || \
 		(test_bit(SPACE, spec->flags) && n >= 0))
 		amount -= 1;
-	if (spec->prec >= 0 && spec->prec > amount_digits(n, spec))
+	if (spec->prec > amount_digits(n, spec))
 		amount += spec->width - spec->prec;
 	else if (test_bit(DOT, spec->flags) && spec->prec <= 0 && n == 0)
 		amount += spec->width;
@@ -93,6 +91,15 @@ static int	print_padding(long long n, t_spec *spec)
 		amount += spec->width - amount_digits(n, spec);
 	ret += putstr_repeat(pad_char, amount);
 	return (ret);
+}
+
+void	set_pad_char(t_spec *spec, char *pad_char)
+{
+	if (test_bit(ZERO, spec->flags) && \
+		(!test_bit(PREC, spec->flags) || spec->prec < 0))
+		ft_strcpy(pad_char, "0\0");
+	else
+		ft_strcpy(pad_char, " \0");
 }
 
 static int	print_prefix(long long n, t_spec *spec)
@@ -134,7 +141,7 @@ static int	print_number(long long n, t_spec *spec)
 		sign = -1;
 	else
 		sign = 1;
-	if (spec->prec != NOT_SET)
+	if (test_bit(PREC, spec->flags))
 		ret += putstr_repeat("0", spec->prec - amount_digits(n, spec));
 	if (spec->specifier == LOWHEX)
 		ret += put_ull_base(n * sign, HEXDIGITSLOW);
