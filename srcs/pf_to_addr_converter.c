c5cf7e6bf0a3e88bb04579901d391a05de910739
Javier <javi@grey.local>
2022-01-22 19:03:54 +0200

    fix 'to_address' converter: now it deals with precision (and lack of it when n == 0)

--- srcs/pf_to_addr_converter.c
+++ srcs/pf_to_addr_converter.c
@@ -6,7 +6,7 @@
 /*   By: rodrodri <rodrodri@student.hive.fi >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 12:24:46 by rodrodri          #+#    #+#             */
-/*   Updated: 2022/01/22 13:58:31 by rodrodri         ###   ########.fr       */
+/*   Updated: 2022/01/22 19:00:47 by rodrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 #include "ft_printf.h"
 #include "pf_converting.h"
 #include "pf_parsing.h"
+
+static int	print_address(unsigned long	n, t_spec *spec);
 
 /*
 **	Prints the address of a variable in hexadecimal, as an 'unsigned long int'.
@@ -34,13 +36,31 @@ int	to_address(va_list args, t_spec *spec)
 		if (!test_bit(MINUS, spec->flags))
 			ret += putstr_repeat(" ", spec->width - addr_width);
 		ret += ft_putstr("0x");
-		ret += put_ull_base(n, HEXDIGITSLOW);
+		ret += print_address(n, spec);
 		if (test_bit(MINUS, spec->flags))
 			ret += putstr_repeat(" ", spec->width - addr_width);
 	}
 	else
 	{
 		ret += ft_putstr("0x");
+		ret += print_address(n, spec);
+	}
+	return (ret);
+}
+
+static int	print_address(unsigned long	n, t_spec *spec)
+{
+	int	ret;
+
+	ret = 0;
+	if (n == 0 && test_bit(DOT, spec->flags) && \
+		(spec->prec == 0 || !test_bit(PREC, spec->flags)))
+		return (ret);
+	else if (n == 0 && test_bit(PREC, spec->flags) && spec->prec > 0)
+		ret += putstr_repeat("0", spec->prec);
+	else
+	{
+		ret += putstr_repeat("0", spec->prec - amount_digits(n, spec));
 		ret += put_ull_base(n, HEXDIGITSLOW);
 	}
 	return (ret);
