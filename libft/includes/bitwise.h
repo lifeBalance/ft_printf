7aaf2df652e91457e7b0ce7683b0dde935ebd17e
Javier <javi@grey.local>
2022-01-31 22:39:56 +0200

    fix some Norm issues in libft

--- libft/includes/bitwise.h
+++ libft/includes/bitwise.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bitwise.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rodrodri <rodrodri@student.hive.fi >       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/31 22:38:56 by rodrodri          #+#    #+#             */
+/*   Updated: 2022/01/31 22:38:59 by rodrodri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef BITWISE_H
 # define BITWISE_H
 
@@ -7,24 +19,15 @@
 # include <stdint.h>
 
 /*
-** Macro definitions.
-*/
-# define NTOBIN(x, y)	_Generic((x),	uint8_t:	uint8_tobin,	\
-										uint16_t:	uint16_tobin,	\
-										default:	uint16_tobin)(x, y)
-
-/*
 **	Function Prototypes.
 */
-uint16_t	create_mask(uint8_t offset);
-uint16_t	msk_grp(uint8_t width, uint8_t pos);
 uint16_t	test_bit(uint8_t pos, uint16_t val);
 void		set_bit(uint8_t pos, uint16_t *val);
 void		clear_bit(uint8_t pos, uint16_t *val);
 void		toggle_bit(uint8_t pos, uint16_t *val);
+uint16_t	create_mask(uint8_t offset);
+uint16_t	msk_grp(uint8_t width, uint8_t pos);
 uint16_t	read_group(uint8_t pos, uint8_t width, uint16_t val);
 uint16_t	write_group(uint8_t pos, uint8_t width, uint16_t val, uint8_t grp);
-char		*uint8_tobin(uint8_t n, uint8_t nibblify);
-char		*uint16_tobin(uint16_t n, uint8_t nibblify);
 
 #endif
--- libft/srcs/bitwise/bitwise.c
+++ libft/srcs/bitwise/bitwise.c
@@ -6,16 +6,11 @@
 /*   By: rodrodri <rodrodri@student.hive.fi >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/18 00:48:12 by rodrodri          #+#    #+#             */
-/*   Updated: 2022/01/08 19:33:13 by rodrodri         ###   ########.fr       */
+/*   Updated: 2022/01/31 22:35:36 by rodrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bitwise.h"
-
-uint16_t	create_mask(uint8_t offset)
-{
-	return (1 << offset);
-}
 
 uint16_t	test_bit(uint8_t pos, uint16_t val)
 {
@@ -35,30 +30,4 @@ void	clear_bit(uint8_t pos, uint16_t *val)
 void	toggle_bit(uint8_t pos, uint16_t *val)
 {
 	*val ^= (1 << pos);
-}
-
-uint16_t	msk_grp(uint8_t width, uint8_t pos)
-{
-	uint16_t mask;
-
-	if (width == 0)
-		return (0);
-	mask = 1;
-	while (--width)
-	{
-		mask <<= 1;
-		mask |= 1;
-	}
-	mask <<= pos;
-	return (mask);
-}
-
-uint16_t	read_grp(uint8_t pos, uint8_t width, uint16_t val)
-{
-	return ((val & msk_grp(pos, width)) >> pos);
-}
-
-uint16_t	write_grp(uint8_t pos, uint8_t width, uint16_t val, uint16_t grp)
-{
-	return ((~msk_grp(width, pos) & val) | (grp << pos));
 }
